20a01783182f5c74d65b796ad113a3e63010e98f
AramLR <aramluck2007@gmail.com>
2026-06-16 20:12:37 -0600

    feat: add sidebar to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,22 @@
 <script lang="ts">
   import "../app.css";
+  import Sidebar from "$lib/components/Sidebar.svelte";
 
   let { children } = $props();
 </script>
 
-<aside></aside>
+<aside>
+  <Sidebar />
+</aside>
 
 <main>
   {@render children()}
 </main>
+
+<style>
+  :global(#root) {
+    display: grid;
+    grid-template-columns: 1fr 6fr;
+    grid-template-rows: 1fr;
+  }
+</style>
