30c752771fd9dea0c3fdcc3648c83b774baee7ad
AramLR <aramluck2007@gmail.com>
2026-06-16 20:12:07 -0600

    feat: add sidebar component

---
+++ src/lib/components/Sidebar.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import type { Route } from "$lib/types/route";
+
+  const routes: Route[] = [
+    {
+      id: "home",
+      displayName: "Home",
+      url: "/",
+    },
+    {
+      id: "about",
+      displayName: "About",
+      url: "/about",
+    },
+  ];
+</script>
+
+<ul class="menu bg-base-200 rounded-box w-56 h-screen">
+  <li class="menu-title">Pogal</li>
+  {#each routes as { displayName, url }}
+    <li><a href={url}>{displayName}</a></li>
+  {/each}
+</ul>
