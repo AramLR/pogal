98a4c2631693b1feb7cec06fd40788bd69601437
AramLR <aramluck2007@gmail.com>
2026-06-17 21:46:48 -0600

    feat: add toast component

---
+++ src/lib/components/Toast.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  export interface Props {
+    type: "success" | "error" | "info" | "warning";
+    message: string;
+  }
+
+  let { type = "info", message }: Props = $props();
+</script>
+
+<div class="toast toast-end">
+  <div
+    class={{
+      alert: true,
+      "alert-success": type == "success",
+      "alert-info": type == "info",
+      "alert-error": type == "error",
+      "alert-warning": type == "warning",
+    }}
+  >
+    <span>{message}</span>
+  </div>
+</div>
