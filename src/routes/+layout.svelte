694774f80fa460633caa73768eedf0428d3dc34c
AramLR <aramluck2007@gmail.com>
2026-06-18 06:25:47 -0600

    style (layout): add responsive design by using drawer component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,74 @@
 <script lang="ts">
   import "../app.css";
-  import Sidebar from "$lib/components/Sidebar.svelte";
+  import type { Route } from "$lib/types/route";
 
   let { children } = $props();
+
+  const routes: Route[] = [
+    {
+      id: "home",
+      displayName: "Home",
+      url: "/",
+    },
+    {
+      id: "about",
+      displayName: "About",
+      url: "/about",
+    },
+  ];
 </script>
 
-<aside>
-  <Sidebar />
-</aside>
+{#snippet navlinks()}
+  {#each routes as { displayName, url }}
+    <li><a href={url}>{displayName}</a></li>
+  {/each}
+{/snippet}
 
-<main class="p-5">
-  {@render children()}
-</main>
-
-<style>
-  :global(#root) {
-    display: grid;
-    grid-template-columns: 1fr 6fr;
-    grid-template-rows: 1fr;
-  }
-</style>
+<div class="drawer">
+  <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+  <div class="drawer-content flex flex-col">
+    <!-- Navbar -->
+    <div class="navbar bg-base-300 w-full">
+      <div class="flex-none lg:hidden">
+        <label
+          for="my-drawer-2"
+          aria-label="open sidebar"
+          class="btn btn-square btn-ghost"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            class="inline-block h-6 w-6 stroke-current"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            ></path>
+          </svg>
+        </label>
+      </div>
+      <div class="mx-2 flex-1 px-2">Pogal</div>
+      <div class="hidden flex-none lg:block">
+        <ul class="menu menu-horizontal">
+          <!-- Navbar menu content here -->
+          {@render navlinks()}
+        </ul>
+      </div>
+    </div>
+    <!-- Page content here -->
+    <div class="p-10">
+      {@render children()}
+    </div>
+  </div>
+  <div class="drawer-side">
+    <label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"
+    ></label>
+    <ul class="menu bg-base-200 min-h-full w-80 p-4">
+      <!-- Sidebar content here -->
+      {@render navlinks()}
+    </ul>
+  </div>
+</div>
