f6f89d583976af50a3d685069d10efc27f9fc61f
AramLR <aramluck2007@gmail.com>
2026-06-17 19:41:19 -0600

    feat: add promptCard component

---
+++ src/lib/components/PromptCard.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import type { Prompt } from "$lib/types/prompt";
+
+  const { title, description, id }: Prompt = $props();
+</script>
+
+<section class="card w-75 card-xs shadow-sm bg-neutral text-neutral-content">
+  <article class="card-body">
+    <h2 class="card-title">{title}</h2>
+    <p>{description}</p>
+    <div class="card-actions justify-end">
+      <a href="/prompts/{id}" class="btn btn-accent">View</a>
+    </div>
+  </article>
+</section>
