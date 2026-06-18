8e8a85e40609aec17f13cd4827d9b4fb4dda1ca1
AramLR <aramluck2007@gmail.com>
2026-06-17 20:40:53 -0600

    feat (prompt detail page): display prompt metadata

--- src/routes/prompt/[id]/+page.svelte
+++ src/routes/prompt/[id]/+page.svelte
@@ -6,4 +6,18 @@
   const prompt = $derived(getPromptById(params.id));
 </script>
 
-<h1>{prompt?.title}</h1>
+{#if prompt}
+  <section class="bg-base-200 h-full rounded-2xl p-6">
+    <article class="flex flex-col gap-7">
+      <h1 class="text-5xl font-bold">{prompt.title}</h1>
+      <p class="text-xl text-justify">{prompt.description}</p>
+      <ul class="list bg-base-100 shadow-md rounded-box">
+        <li class="p-4 pb-2 text-xs opacity-60 tracking-wide">Use cases</li>
+
+        {#each prompt.useCases as useCase}
+          <li class="list-row">{useCase}</li>
+        {/each}
+      </ul>
+    </article>
+  </section>
+{/if}
