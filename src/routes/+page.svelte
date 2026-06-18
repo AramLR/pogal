b49bd8957a6bafafd76e0498e87cf660e172543e
AramLR <aramluck2007@gmail.com>
2026-06-18 05:13:09 -0600

    feat: add searchbar component to main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,31 @@
 <script lang="ts">
   import PromptCard from "$lib/components/PromptCard.svelte";
   import { getAllPrompts } from "$lib/services/promptService";
+  import SearchBar from "$lib/components/SearchBar.svelte";
 
   const allPrompts = getAllPrompts();
+  let searchInput = $state("");
+  let formattedSearchInput = $derived(searchInput.toLowerCase());
+
+  let selectedPrompts = $derived(
+    allPrompts.filter(
+      (prompt) =>
+        prompt.description.toLowerCase().includes(formattedSearchInput) ||
+        prompt.title.toLowerCase().includes(formattedSearchInput),
+    ),
+  );
 </script>
 
-{#if allPrompts.length > 0}
+<SearchBar bind:input={searchInput} />
+
+{#if selectedPrompts.length > 0}
   <div class="flex flex-wrap items-stretch gap-4">
-    {#each allPrompts as prompt}
+    {#each selectedPrompts as prompt}
       <PromptCard {...prompt} />
     {/each}
   </div>
 {:else}
-  <div class="flex justify-center items-center h-full">
+  <div class="flex justify-center items-center">
     <p class="text-warning text-2xl font-extrabold block m-auto">
       No prompts found
     </p>
