a8bc04186f8f7f3e6825c63621b533676f378db5
AramLR <aramluck2007@gmail.com>
2026-06-17 20:15:23 -0600

    feat: add dynamic route for prompt detail page

---
+++ src/routes/prompt/[id]/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import type { PageProps } from "./$types";
+  import { getPromptById } from "$lib/services/promptService";
+
+  const { params }: PageProps = $props();
+  const prompt = $derived(getPromptById(params.id));
+</script>
+
+<h1>{prompt?.title}</h1>
